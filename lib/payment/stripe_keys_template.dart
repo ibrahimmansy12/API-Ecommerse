165a80ca098763c895aec62cccaa29245647b5af
ibrahimmansy12 <your-email@example.com>
2025-07-07 14:56:01 +0300

    Add ApiKeys class for Stripe payment integration

---
+++ lib/payment/stripe_keys_template.dart
@@ -0,0 +1,5 @@
+// payment/stripe_keys_template.dart
+abstract class ApiKeys {
+  static const String pusblishableKey = "YOUR_PUBLISHABLE_KEY_HERE";
+  static const String secretKey = "YOUR_SECRET_KEY_HERE";
+}
